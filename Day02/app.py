520acb0112f26ceedbf2cdcb03c78d28aac04245
Roberto Cucinotta <4200884+robbixc@users.noreply.github.com>
2025-03-31 13:05:16 +0200

    aggiungo struttura per day2

---
+++ Day02/app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Creo un oggetto Path
+path = Path("Day02/input.txt")
+
+# Apro il file in lettura
+with open(path, "r") as file:
+    print("TBD")
+    
+
+print("\nPROGRAMMA TERMINATO")    
