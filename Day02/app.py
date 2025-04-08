6117a5017fffac49d59ef1fd651a716ed931703f
Deflagratore <fnicodemo2007@gmail.com>
2025-04-08 15:00:52 +0200

    nuova possibile soluzione day2 (manca revisione)

--- Day02/app.py
+++ Day02/app.py
@@ -4,15 +4,9 @@ import funzioni_day2 as fd
 # Creo un oggetto Path
 path = Path("Day02/input.txt")
 
-#creo una lista per gli id dei giochi
-lista_ids = []
-
 # Apro il file in lettura
 with open(path, "r") as file:
     righe = file.readlines()
-
-    #creo una lista degli game id coretti
-    lista_game_id_corretti = []
 
     #creo un dizionario cosi che le ogni partita venga affibbiata col proprio game id
     lista_partite = {}
@@ -22,24 +16,14 @@ with open(path, "r") as file:
 
         #prendiamo il game id
         game_id = fd.get_game_id(riga)
-        lista_ids.append(game_id)
 
         lista_partite[game_id] = fd.get_information(riga)
         print(lista_partite[game_id])
 
-
-    """allora io ho provato a cancellare i game id dove le partite venivano perse ma non ci sono riuscito benissimo
-    credo che ci siano ancora degli errori poi ce da dare una controllata"""
-    for game_id in lista_ids:      
-        # non capisco questo controllo, nella lista partite così come l'hai composto le chiavi sono i numeri di cubi
-        # e i valori sono i colori... quindi al massimo dobbiamo controllare i numeri dei cubi.
-        # Poi il range non corrisponde a quanto scritto nelle istruzioni. Dobbiamo controllare che non vengano mai estratti
-        # più di 12 cubi rossi oppure 13 cubi verdi oppure 14 cubi blu, altrimenti la partita non è valida
-        if lista_partite[game_id].values() in range(15,21):
-            lista_partite.pop(game_id)
-        else:
-            lista_game_id_corretti.append(game_id)
-
-print(sum(lista_game_id_corretti))
+    #filtro i game id e creo una lista con i game id che soddisfano le condizioni
+    lista_ids_coretti = fd.filter_game_id(lista_partite)
+    
+    print("\n\nLista dei game id corretti: %s" % (lista_ids_coretti))
+    print("\nLa somma dei game id corretti è: %s" % (sum(lista_ids_coretti)))
 
 print("\nPROGRAMMA TERMINATO")    
--- Day02/funzioni_day2.py
+++ Day02/funzioni_day2.py
@@ -19,7 +19,7 @@ def remove(text: str) -> str:
     new_text = re.sub(pattern, "", text)
     return new_text
 
-def get_information(text: str) -> dict[int, list[int]]:
+def get_information(text: str) -> dict[str, list[int]]:
     """Questa funzione analizza la stringa alla ricerca di due parametri di cui uno e un numero e l'altro il nome
     di un colore, poi una volta trovati la funzione crea un dizionario con chiave il colo9re e come valore la lista dei numeri\n
     :param: una stringa di testo
@@ -34,3 +34,31 @@ def get_information(text: str) -> dict[int, list[int]]:
         else:
             dizionario[match[2]] = list([int(match[1])])
     return dizionario 
+
+def filter_game_id(partite: dict[int, dict[str, list[int]]]) -> list[int]:
+    """Questa funzione filtra i game id in base a tre condizioni:\n
+    1. Se il colore rosso contiene un numero compreso tra 12 e 20, il game id viene escluso.
+    2. Se il colore verde contiene un numero compreso tra 13 e 20, il game id viene escluso.
+    3. Se il colore blu contiene un numero compreso tra 14 e 20, il game id viene escluso.\n
+    :param: un dizionario con i game id e i colori
+    :return: una lista di game id che soddisfano le condizioni"""
+    lista_ids_coretti = []
+
+    lista1 = set(range(12, 21))
+    lista2 = set(range(13, 21))
+    lista3 = set(range(14, 21))
+
+    for game_id, colori in partite.items():
+        if colori: # Verifica se il dizionario non è vuoto
+            if any(value in lista1 for value in colori.get("red", [])):
+                continue
+            if any(value in lista2 for value in colori.get("green", [])):
+                continue
+            if any(value in lista3 for value in colori.get("blue", [])):
+                continue
+        
+        lista_ids_coretti.append(game_id)
+
+    return lista_ids_coretti
+
+
